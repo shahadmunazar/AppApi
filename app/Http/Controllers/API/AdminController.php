54a5944cc7a50b5d6cce878fe865980534b9a17d
hackerkernel15@gmail.com <shahadmunazar@gmail.com>
2024-07-15 22:45:48 +0530

    Update AdminController.php

--- app/Http/Controllers/API/AdminController.php
+++ app/Http/Controllers/API/AdminController.php
@@ -3,25 +3,23 @@
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-
 
 class AdminController extends Controller
 {
     public function getAdmin()
     {
         try {
-            $user  = Auth::user();
+            $user = Auth::user();
             return response()->json(['status' => 200, 'data' => $user]);
         } catch (\Throwable $th) {
-            //throw $th;
+            return response()->json(['status' => 500, 'error' => "SomeThign Went Wrong Please try Again later"]);
         }
     }
     public function profile()
     {
         try {
-            $user  = Auth::user();
+            $user = Auth::user();
             return response()->json(['status' => 200, 'data' => $user]);
         } catch (\Throwable $th) {
             dd($th);
