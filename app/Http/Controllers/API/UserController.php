b362ba31800863eeb770b1eb82e17dc6393781d6
hackerkernel15@gmail.com <shahadmunazar@gmail.com>
2024-07-15 22:48:54 +0530

    Update UserController.php

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
 class UserController extends Controller
@@ -11,9 +10,11 @@ class UserController extends Controller
     public function dashboard()
     {
         try {
-            $user  = Auth::user();
+            $user = Auth::user();
             return response()->json(['status' => 200, 'data' => $user]);
         } catch (\Throwable $th) {
+            return response()->json(['status' => 500, 'error' => "SomeThign Went Wrong Please try Again later"]);
+
             dd($th);
             //throw $th;
         }
