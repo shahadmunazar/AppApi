6656db373e0ec2a329ecbb78542eafd481713aea
Shahad <shahadmunazar@gmail.com>
2026-09-03 16:54:08 +0530

    User fillable

--- app/Models/User.php
+++ app/Models/User.php
@@ -18,7 +18,7 @@ class User extends Authenticatable
      * @var array<int, string>
      */
     protected $fillable = [
-        'name', 'email', 'mobile', 'password', 'referrer_id','status', 'referral_code', 'role'
+        'name', 'email', 'mobile', 'password', 'referrer_id','status', 'referral_code', 'role', 'balance', 'bonus_balance', 'deposit_balance', 'winning_balance'
     ];
 
     public function referrals()
